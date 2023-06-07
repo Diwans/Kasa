359141f7a9ab77dc91846d483dc99cf4958674f9
Diwan Schneider <diwanschneide@yahoo.fr>
2023-06-07 11:30:44 +0200

    ajout de la page logement

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import{ BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Logement from './pages/Logement';
 
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/About" element={<About/>}/>
-          <Route path="*" element={<Error/>}/>
+          <Route path="/" element={<Home/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Logement" element={<Logement/>} />
+          <Route path="*" element={<Error/>} />
         </Routes>
       </BrowserRouter>
     );
