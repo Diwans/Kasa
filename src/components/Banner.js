3721aa2faf95fccc1c540987b4de1bcd4104de7b
Diwan Schneider <diwanschneide@yahoo.fr>
2023-06-08 12:11:46 +0200

    fin de la banner desktop

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/banner.scss'
 
 const Banner = () => {
     return (
         <div id='banner'>
-            
+            <div className='bannerTxt'>Chez vous, partout et ailleurs</div>
         </div>
     );
 };
